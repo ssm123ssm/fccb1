bd4c2219aea20fc918f163cc5a2dfb4593e1bbaf
ssm123ssm <ssm123ssm@gmail.com>
2017-06-07 13:07:08 +0530

    final

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-//var express = require('express');
-const express = require('express')
-const app = express()
+var express = require('express');
+var dateformat = require('dateformat');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get("/:val", function(req, res){
+    var val = req.params.val;
+    var unix = + new Date(val);
+    var natural;
+    var ret = {unix:null, natural:null};
+    console.log(val);
+    if(unix){
+        console.log("natural entered");
+        console.log(+ new Date(val));    
+        natural = dateformat(unix, "mmmm dS, yyyy");
+        console.log(natural);
+        ret.unix = unix;
+        ret.natural = natural;
+    }
+    else{
+        if(isNumeric(val)){
+            console.log("unix entered");
+            natural = dateformat(Number(val), "mmmm dS, yyyy");
+            console.log(natural);
+            ret.unix = Number(val);
+            ret.natural = natural;
+        }
+        else{
+            console.log("invalid");
+        }
+        
+    }
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    res.send(ret);
+    
+});
+
+app.listen(80);
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
